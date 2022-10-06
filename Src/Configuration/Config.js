8717d1c7eea343094cb157d8de3a7454c307ffaa
Abhijit <devabhijit11@gmail.com>
2022-10-07 00:10:38 +0530

    Update Config.js

--- Src/Configuration/Config.js
+++ Src/Configuration/Config.js
@@ -1,10 +1,10 @@
 const { ButtonStyle } = require("discord.js");
 require("dotenv").config();
 module.exports = {
-    Token: process.env.Token || "MTAxODg5MDU3MzYyNjc0NDg0Mg.G80yg7.Swqe3xu5oqHmUtUm5dSVWWTQ-_M9GFLlkbvLKI", //Here Goes Your TOKEN
+    Token: process.env.Token || "", //Here Goes Your TOKEN
     Prefix: process.env.Prefix || "?", //Here Goes Your Prefix
     Client: {
-        ID: process.env.ClientID || "937003232474046554", //Here Goes Your Bot Client Id
+        ID: process.env.ClientID || "", //Here Goes Your Bot Client Id
         Secret: process.env.ClientSecret || "" //Here Goes Your Bot Client Secret
     },
     button: {
@@ -16,13 +16,13 @@ module.exports = {
             "green": ButtonStyle.Success
         }
     },
-    MongoData: process.env.MongoDB || "mongodb+srv://Apera:6291@cluster0.7qkb4.mongodb.net/aperaCanary", //Here Goes Your MongoDb Url
+    MongoData: process.env.MongoDB || "", //Here Goes Your MongoDb Url
     EmbedColor: process.env.EmbedColor || "#10a464", //Here Goes Your EmbedColor
     Owners: ["588659781930188811"],
     links: {
-        invite: "https://moebot.xyz/vote",
-        bg: "https://moebot.xyz/vote",
-        support: "https://moebot.xyz/vote",
-        vote: "https://moebot.xyz/vote"
+        invite: "https://github.com/Fluid-Devs/V14-handler",
+        bg: "https://github.com/Fluid-Devs/V14-handler",
+        support: "https://github.com/Fluid-Devs/V14-handler",
+        vote: "https://github.com/Fluid-Devs/V14-handler"
     }
 }
